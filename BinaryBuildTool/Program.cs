513766db043eb60d610e7af9530c9125be060507
dagraac <dagraac@naver.com>
2023-06-15 20:39:52 +0900

    work

--- BinaryBuildTool/Program.cs
+++ BinaryBuildTool/Program.cs
@@ -14,25 +14,27 @@ namespace BinaryBuildTool
 		public class TableData
 		{
 			public int id;
-			public string value;
-			public bool isFlag;
-			public float position;
+			//public string value;
+			//public bool isFlag;
+			//public float position;
 		}
 
 
+		/// <summary>
+		/// 시작됨.
+		/// </summary>
 		public static void Main(string[] args)
 		{
-			var originalDatas = new List<TableData>();
-
-			const int maxCount = 1000;
+			var maxCount = 1000;
+			var originalDatas = new List<TableData>(maxCount);
 			for (var i = 0; i < maxCount; ++i)
 			{
 				var tableData = new TableData
 				{
 					id = i + 1,
-					value = i.ToString(),
-					isFlag = i % 2 == 0,
-					position = 15.5678f,
+					//value = i.ToString(),
+					//isFlag = i % 2 == 0,
+					//position = 15.5678f,
 				};
 
 				originalDatas.Add(tableData);
@@ -41,41 +43,21 @@ namespace BinaryBuildTool
 			var startTime = default(DateTime);
 			var endTime = default(DateTime);
 
-			//// BPObject로 변환.
-			//startTime = DateTime.Now;
-			//var bpObject = BPConvert.Serialize(originalDatas);
-			//endTime = DateTime.Now;
-			//Console.WriteLine("BPObject Serialize Time = {0:F4}s", (endTime - startTime).TotalSeconds);
+			// 과정.
+			// Serialize : Object ==> (BPObject ==> ByteArray)
+			// Deserialize : (ByteArray ==> BPObject) ==> Object
 
-			//// 원래의 데이터로 변환.
-			//startTime = DateTime.Now;
-			//var newDatas = BPConvert.Deserialize<List<TableData>>(bpObject);
-			//endTime = DateTime.Now;
-			//Console.WriteLine("BPObject Deserialize Time = {0:F4}s", (endTime - startTime).TotalSeconds);
-
-			//// 바이트배열로 변환.
-			//startTime = DateTime.Now;
-			//var bytes = bpObject.ObjectToBytes();
-			//endTime = DateTime.Now;
-			//Console.WriteLine("BPObject Object To Bytes Time = {0:F4}s", (endTime - startTime).TotalSeconds);
-
-			//// BPObject로 변환.
-			//startTime = DateTime.Now;
-			//var bpNewObject = bytes.BytesToObject();
-			//endTime = DateTime.Now;
-			//Console.WriteLine("BPObject Byte To Object Time = {0:F4}s", (endTime - startTime).TotalSeconds);
-			
-			// 데이터를 바이트로 변환.
+			// 데이터를 바이트배열로 변환.
 			startTime = DateTime.Now;
-			var bytes = BPConvert.Serialize<List<TableData>>(originalDatas);
+			var byteArray = BPConvert.Serialize<List<TableData>>(originalDatas);
 			endTime = DateTime.Now;
-			Console.WriteLine("Data to Bytes Time = {0:F4}s", (endTime - startTime).TotalSeconds);
+			Console.WriteLine("Data to ByteArray Time = {0:F4}s", (endTime - startTime).TotalSeconds);
 
-			// 바이트를 데이터로 변환.
+			// 바이트배열을 데이터로 변환.
 			startTime = DateTime.Now;
-			var newDatas = BPConvert.Deserialize<List<TableData>>(bytes);
+			var newDatas = BPConvert.Deserialize<List<TableData>>(byteArray);
 			endTime = DateTime.Now;
-			Console.WriteLine("Bytes to Data Time = {0:F4}s", (endTime - startTime).TotalSeconds);
+			Console.WriteLine("ByteArray to Data Time = {0:F4}s", (endTime - startTime).TotalSeconds);
 
 			// 완료 후 대기.
 			Console.ReadKey();
